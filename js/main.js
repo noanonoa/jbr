d4f660255067a20355a6951f0b9a5c29da0b581b
Noa Kim <noanonoa@gmail.com>
2022-06-21 22:31:06 -0500

    add footer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('click', (e) => {
   const { id } = e.target
   id == 'menu-button' ? showMenu(true) : showMenu(false)
 })
+
 function showMenu(toggle){
   const menu = document.querySelector('nav ul')
   if (toggle == true){
